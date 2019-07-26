eaa0e998f12b73428cc78b3cf64ac4ac5cfb8574
dbezrukov <dmitry.v.bezrukov@gmail.com>
2019-07-26 12:26:00 +0300

    adding helper-map

---
+++ helpers/helpers-map.js
@@ -0,0 +1,12 @@
+// In js we should avoid mutating original data 
+// so it is a good practice to return new array
+
+var trips = [
+    { distance: 34, time: 10 },
+    { distance: 90, time: 50 },
+    { distance: 59, time: 25 }
+];
+
+var speeds = trips.map(function(trip) {
+    return (trip.distance / trip.time);
+})
