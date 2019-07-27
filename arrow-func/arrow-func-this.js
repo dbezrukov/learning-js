095106b379c4f3a4192ad02be4537befffba0efc
dbezrukov <dmitry.v.bezrukov@gmail.com>
2019-07-27 09:12:29 +0300

    adding another arrow func example

--- arrow-func/arrow-func-this.js
+++ arrow-func/arrow-func-this.js
@@ -10,14 +10,24 @@ const team = {
 
 team.teamSummary(); // loosing context: this.teamName will be undefined in ES5 functions
 
+// ES6 arrow function uses 'this' from the surrounding context
 const team = {
   members: ['Jane', 'Bill'],
   teamname: 'Super Team',
   teamSummary: function() {
     return this.members.map(member => {
-      return `${member} is on team ${this.teamName}`; // this equals to 'team'
+      return `${member} is on team ${this.teamName}`; // this equals to the 'team' object
     })
   }
 }
 
-// ES6 arrow function uses 'this' from the surrounding context
+// Unexpected behaviour when using arrow functions
+const profile = {
+  name: 'Alex',
+  getName: function() { return this.name },
+  getNameArrow: () => { return this.name } // this is not defined here
+};
+
+profile.getName(); // 'function' keyword should be used
+profile.getNameArrow();
+
