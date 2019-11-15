19528431d1d678286fc6f7e8436eb4b461ddd5a7
Dmitry Bezrukov <dmitry.v.bezrukov@gmail.com>
2019-11-15 10:35:17 +0300

    Update types.js

--- types/types.js
+++ types/types.js
@@ -11,4 +11,19 @@ And 7 primitives data types:
 - BigInt
 - String
 - Symbol
+
+Objects are:
+- "Normal" objects, and functions
+- Dates
+- Indexed collections: Arrays and typed Arrays
+  Typed Arrays presents an array-like view of an underlying binary data buffer. 
+- Keyed collections: Maps, Sets, WeakMaps, WeakSets
+  Set and WeakSet represent a set of objects, while Map and WeakMap associate a value to an object. 
+  The difference between Maps and WeakMaps is that in the former, object keys can be enumerated over. 
+  This allows garbage collection optimizations in the latter case.
+- Structured data: JSON
+
+More info on objects: 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+
 */
