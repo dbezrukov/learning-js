2469cc701a361588e37313c3dce7716595cd4b74
Dmitry Bezrukov <dmitry.v.bezrukov@gmail.com>
2019-11-20 12:10:53 +0300

    Update arrow-func-brevity.js

--- arrow-func/arrow-func-brevity.js
+++ arrow-func/arrow-func-brevity.js
@@ -5,7 +5,7 @@ const double = (number) => {
 // braces can be omitted for a single expression
 const double = (number) => 2 * number;
 
-// parent can be omitted for a single agrument
+// parenthesis can be omitted for a single agrument
 const double = number => 2 * number;
 
 const numbers = [1, 2, 3];
